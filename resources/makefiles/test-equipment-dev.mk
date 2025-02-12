42ed58e03e377d82668934ab6b1e3f5c13ebb591
Johan Coetzer <johan.coetzer@tsolo.io>
2025-02-12 10:47:51 +0200

    AT-2686 another attempt at AWG amazement

--- resources/makefiles/test-equipment-dev.mk
+++ resources/makefiles/test-equipment-dev.mk
@@ -6,7 +6,7 @@ ifeq ($(CI_JOB_NAME),deploy-test-equipment-for-verification) # if CI_JOB_NAME is
 # # Set K8S_EXTRA_PARAMS for deploying Test Equipment during development of the Test Equipment charts
 TE_REGISTRY ?= registry.gitlab.com/ska-telescope/ska-ser-test-equipment
 TE_IMAGE ?= ska-ser-test-equipment
-COMMIT_HASH ?= 251be106
+COMMIT_HASH ?= 279cfa31
 #TE_VERSION ?= 0.9.1 # this line can be commented out or overwritten by the following line
 TE_VERSION ?= 0.9.1-dev.c$(COMMIT_HASH)  # This is the version of the image that we want to pull from https://gitlab.com/ska-telescope/ska-ser-test-equipment/container_registry/3213235
 
